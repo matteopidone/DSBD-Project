b191011c841eb1469989c626a0ea605e9da7ab92
Tomas Prifti <tomasprifti99@gmail.com>
2023-01-13 18:21:14 +0100

    feat(service_1 - alpine): modificato pip con pip3.10 e aggiunti print (alpine non funzionante)

--- service_1/application/main.py
+++ service_1/application/main.py
@@ -1,7 +1,11 @@
+print("Service_1")
+print("Before import libraries")
+
 from prometheus_api_client import PrometheusConnect, MetricsList, MetricSnapshotDataFrame, MetricRangeDataFrame
 from datetime import timedelta
 from prometheus_api_client.utils import parse_datetime
 
+print("Starting script")
 prom = PrometheusConnect(url="http://15.160.61.227:29090/", disable_ssl=True)
 label_config = {'job': 'summary'}
 queryResult = prom.get_current_metric_value(metric_name='availableMem', label_config=label_config)
