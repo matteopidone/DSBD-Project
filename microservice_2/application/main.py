51b9daffa32e2e985659105e4596d8270c07a97d
Tomas Prifti <tomasprifti99@gmail.com>
2023-01-14 18:25:29 +0000

    feat(microservice_2): gestione accesso al database mysql, con esempi di create, insert e select

--- microservice_2/application/main.py
+++ microservice_2/application/main.py
@@ -1,10 +1,23 @@
 import mysql.connector
 
+#https://www.w3schools.com/python/python_mysql_getstarted.asp
+
 print("Start")
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="password"
+  password="password",
+  database="database"
 )
 
 print(mydb)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers"
+mycursor.execute(sql)
+
+result = mycursor.fetchall()
+
+for x in result:
+    print(x)
