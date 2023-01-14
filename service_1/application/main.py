05723ba81c6c925b84058a445bc92c2a1f89ec51
Matteo Pidone <pidonematteo@hotmail.it>
2023-01-14 12:49:26 +0100

    feat(get metrics): init filtraggio metriche raccolte da prometheus

--- service_1/application/main.py
+++ service_1/application/main.py
@@ -3,6 +3,12 @@ from datetime import timedelta
 from prometheus_api_client.utils import parse_datetime
 
 import json
+def is_subset(a, b):
+    subset = {}
+    for k, v in a.items():
+        if k in b:
+            subset[k] = b[k]
+    return subset == a
 
 # try and catch da inserire
 
@@ -17,11 +23,11 @@ queryResult = prom.custom_query(query='{job="' + data['job_name'] +'"}')
 for metric_info in queryResult :
     for metrics in data['metrics']:
         if metric_info['metric']['__name__'] == metrics['metric_name']:
-            metric_info_json = json.loads(metric_info)
-            print(metric_info_json)
-
+            if( is_subset( metrics['metadata'], metric_info['metric'] ) ):
+                print(metric_info['metric'])
 #metric_df = MetricSnapshotDataFrame(queryResult)
 #print(metric_df)
 #print(metric_df["__name__"][1])
 #print(metric_df["timestamp"][1])
+
 #print(metric_df["value"][1])
