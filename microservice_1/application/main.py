a9f616376e8978ef82d0e61cff8d3050c5f342c0
Tomas Prifti <tomasprifti99@gmail.com>
2023-01-25 15:27:37 +0100

    feat(microservice_1): inizio implementazione algoritmi

--- microservice_1/application/main.py
+++ microservice_1/application/main.py
@@ -1,33 +1,34 @@
-from prometheus_api_client import PrometheusConnect, MetricsList, MetricSnapshotDataFrame, MetricRangeDataFrame
-from datetime import timedelta
-from prometheus_api_client.utils import parse_datetime
-import sys
-import json
+from prometheus_api_client import PrometheusConnect
 from MessageProducer import MessageProducerClass
 from threading import Thread
+import json
+import os
 
 """ Main Function """
 
 def main():
-    # try and catch da inserire
-    file = open("../config.json")
-    data = json.load(file)
+
+    try:
+        file = open("../config.json", "r")
+    except FileNotFoundError:
+        print("File not found.")
+
+    data = json.load(file) 
     file.close()
 
     metrics_list = list()
-    prom = PrometheusConnect(url="http://15.160.61.227:29090/", disable_ssl=True) # togliere l'url e metterlo in env
+    prom = PrometheusConnect(url=os.environ['PROMETHEUS_SERVER'], disable_ssl=True)
     queryResult = prom.custom_query(query='{job="' + data['job'] +'"}[' + data['range_time'] +']')
 
-    for metricResultQuery in queryResult :
+    for metricResultQuery in queryResult:
         for metric in data['metrics']:
             if metricResultQuery['metric']['__name__'] == metric['name']:
-                if( is_subset( metric['labels'], metricResultQuery['metric'] ) ):
-                    #metriche desiderate
+                if is_subset( metric['labels'], metricResultQuery['metric']):
                     t1 = Thread(target=calculate_stats_values, args=(metricResultQuery,))
+                    t2 = Thread(target=calculate_prediction_values, args=(metricResultQuery,))
+                    t3 = Thread(target=calculate_metadata_values, args=(metricResultQuery,))
                     t1.start()
-                    t2 = Thread(target=calculate_prediction_values(metrics_list), args=(metricResultQuery,))
                     t2.start()
-                    t3 = Thread(target=calculate_metadata_values(metrics_list), args=(metricResultQuery,))
                     t3.start()
                     t1.join()
                     t2.join()
@@ -44,13 +45,29 @@ def is_subset(a, b):
     return subset == a
 
 def calculate_stats_values(metric, hours='1h'):
+    print('inside calculate_stats_values')
+    print(metric)
+    
+    max = min = avg = dev_std = sum = n = 0
+
+    for timestamp, value in metric:
+        if value > max:
+            max = value
+        elif value < min:
+            min = value
+        sum += value
+        n += 1
+
+    avg = sum/n
+
     #Calcoli
     #Record Key Stats#indice_metrica
     data = {
         'name': 'pippo',
         'max': 10,
         'min': 1,
-        'avg': 7
+        'avg': 7,
+        'dev_std': 2
     }
     resp = message_producer.send_msg(data)
     print(resp)
@@ -61,8 +78,9 @@ def calculate_metadata_values(metric, hours='1h'):
     #Record Key Metadata#indice_metrica
     data = {
         'name': 'pippo',
-        'dev_std': 10,
-        'autocorrelazione': 1
+        'autocorrelazione': 1,
+        'stazionarieta': 4,
+        'stagionalita': 8
     }
     resp = message_producer.send_msg(data)
     print(resp)
@@ -76,7 +94,6 @@ def calculate_prediction_values(metric, hours='1h'):
         'max': 10,
         'min': 1,
         'avg': 7,
-        'dev_std': 4
     }
     resp = message_producer.send_msg(data)
     print(resp)
