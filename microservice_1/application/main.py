db16bc3b645d86dec7efa621db6a3899f9be63da
Tomas Prifti <tomasprifti99@gmail.com>
2023-01-25 17:30:48 +0100

    feat(microservice_1): modificata struttura script e aggiunte variabili di ambiente
    Il calcolo dei metadati per ciascuna metrica viene fatto solamente una volta. Successivamente ogni
    intervallo di tempo fissato vengono calcolate le statistiche e le predizioni (da implementare)

--- microservice_1/application/main.py
+++ microservice_1/application/main.py
@@ -1,6 +1,7 @@
 from prometheus_api_client import PrometheusConnect
 from MessageProducer import MessageProducerClass
 from threading import Thread
+from time import sleep
 import json
 import os
 
@@ -16,24 +17,41 @@ def main():
     data = json.load(file) 
     file.close()
 
-    metrics_list = list()
     prom = PrometheusConnect(url=os.environ['PROMETHEUS_SERVER'], disable_ssl=True)
+    
+    """ Start metadata calculus """
     queryResult = prom.custom_query(query='{job="' + data['job'] +'"}[' + data['range_time'] +']')
 
     for metricResultQuery in queryResult:
         for metric in data['metrics']:
             if metricResultQuery['metric']['__name__'] == metric['name']:
                 if is_subset( metric['labels'], metricResultQuery['metric']):
-                    t1 = Thread(target=calculate_stats_values, args=(metricResultQuery,))
-                    t2 = Thread(target=calculate_prediction_values, args=(metricResultQuery,))
-                    t3 = Thread(target=calculate_metadata_values, args=(metricResultQuery,))
-                    t1.start()
-                    t2.start()
-                    t3.start()
-                    t1.join()
-                    t2.join()
-                    t3.join()
+                    t0 = Thread(target=calculate_metadata_values, args=(metricResultQuery['metric'], metricResultQuery['values']))
+                    t0.start()
                     break
+
+    sleep_time = 600
+    if os.environ.get('INTERVAL_TIME_SECONDS'):
+        sleep_time = os.environ['INTERVAL_TIME_SECONDS']
+
+    """ Start statistics and predictions calculus """
+    while True:
+        for metric in data['metrics']:
+
+            t1 = Thread(target=calculate_stats_values, args=(prom, metric, '1h'))
+            t2 = Thread(target=calculate_stats_values, args=(prom, metric, '3h'))
+            t3 = Thread(target=calculate_stats_values, args=(prom, metric, '12h'))
+            t1.start()
+            t2.start()
+            t3.start()
+            t4 = Thread(target=calculate_prediction_values, args=(prom, metric, '1h'))
+            t5 = Thread(target=calculate_prediction_values, args=(prom, metric, '3h'))
+            t6 = Thread(target=calculate_prediction_values, args=(prom, metric, '12h'))
+            t4.start()
+            t5.start()
+            t6.start()
+
+        sleep(sleep_time)
 
 """ Functions """
 
@@ -44,10 +62,28 @@ def is_subset(a, b):
             subset[k] = b[k]
     return subset == a
 
-def calculate_stats_values(metric, hours='1h'):
+""" Metadata calculus """
+def calculate_metadata_values(metric_info, values):
+
+
+
+    data = {
+        'name': 'pippo',
+        'autocorrelazione': 1,
+        'stazionarieta': 4,
+        'stagionalita': 8
+    }
+    resp = message_producer.send_msg(data)
+
+""" Statistics calculus """
+def calculate_stats_values(prom, metric, hours='1h'):
     print('inside calculate_stats_values')
     print(metric)
     
+    #queryResult = prom.custom_query(query='{job="' + data['job'] +'"}[' + data['range_time'] +']')
+
+
+
     max = min = avg = dev_std = sum = n = 0
 
     for timestamp, value in metric:
@@ -70,23 +106,9 @@ def calculate_stats_values(metric, hours='1h'):
         'dev_std': 2
     }
     resp = message_producer.send_msg(data)
-    print(resp)
-    return resp
 
-def calculate_metadata_values(metric, hours='1h'):
-    #Calcoli
-    #Record Key Metadata#indice_metrica
-    data = {
-        'name': 'pippo',
-        'autocorrelazione': 1,
-        'stazionarieta': 4,
-        'stagionalita': 8
-    }
-    resp = message_producer.send_msg(data)
-    print(resp)
-    return resp
-
-def calculate_prediction_values(metric, hours='1h'):
+""" Predictions calculus """
+def calculate_prediction_values(prom, metric, hours='1h'):
     #Calcoli
     #Record Key Prediction#indice_metrica
     data = {
@@ -96,13 +118,11 @@ def calculate_prediction_values(metric, hours='1h'):
         'avg': 7,
     }
     resp = message_producer.send_msg(data)
-    print(resp)
-    return resp
 
 """ Start Main Script """
 
 if __name__ == '__main__':
-    broker = "kafka:9092"
-    topic = "prova"
+    broker = os.environ['KAFKA_BROKER']
+    topic = os.environ['KAFKA_TOPIC']
     message_producer = MessageProducerClass(broker, topic)
     main()
