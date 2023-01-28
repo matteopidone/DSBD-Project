e50c570c60a3d7a485e7acef969b13b8c4c7d9d1
Tomas Prifti <tomasprifti99@gmail.com>
2023-01-28 10:10:46 +0100

    feat(microservice_4): creazione template con form per la selezione delle metriche nel SLA

--- microservice_4/application/main.py
+++ microservice_4/application/main.py
@@ -1,8 +1,7 @@
-from flask import Flask
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def template_sla_manager():
+    return render_template('file.html', results=['cpu', 'memory', 'network'])
