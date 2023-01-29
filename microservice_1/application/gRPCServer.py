e504a45e5ffac6cae416d2c1e4d5f8a263283671
Matteo Pidone <pidonematteo@hotmail.it>
2023-01-29 20:02:28 +0100

    fix(microservizio 1): grpc server
    se non ho i dati pronti, restituisco stringa vuota

--- microservice_1/application/gRPCServer.py
+++ microservice_1/application/gRPCServer.py
@@ -11,6 +11,7 @@ class EchoService(echo_pb2_grpc.EchoServiceServicer):
     messagge = None
     def __init__(self, queue) :
         self.queue = queue
+        self.message = ''
         super().__init__()
         ''' Message expected from Etl_Data_pipeline
         self.message = {
@@ -36,19 +37,17 @@ class EchoService(echo_pb2_grpc.EchoServiceServicer):
 
 
     def getNumberOfViolationsPast(self, request, context) :
-        ''' Gestire
-        if (self.queue.empty() and self.messagge == None) :
-            print("empty")
-            return echo_pb2.resultValue(result='Error') 
-        '''
         while not (self.queue.empty()) :
             self.message = self.queue.get()
 
-        calculator_instance = MetricCalculator()
-        result = calculator_instance.get_number_violation(request.listMetrics, str(self.message))
-        
-        return echo_pb2.resultValue(result=str(result))
-
+        #Se l'etl ha prodotto i dati calcolo le violazioni, altrimenti invio messaggio di errore
+        if self.message :
+            calculator_instance = MetricCalculator()
+            result = calculator_instance.get_number_violation(request.listMetrics, str(self.message))
+            print("Risultato " + str(result))
+            return echo_pb2.resultValue(result=str(result))
+        else : 
+            return echo_pb2.resultValue(result='')
 def serve(s):
     port = '50051'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
