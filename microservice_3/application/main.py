afaebadc531975d70fc5f9f6bda6d5f893db1af8
Tomas Prifti <tomasprifti99@gmail.com>
2023-01-29 18:49:23 +0100

    feat(microservice_3): creazione nuova route per la visualizzazione delle predizioni di una metrica

--- microservice_3/application/main.py
+++ microservice_3/application/main.py
@@ -9,11 +9,7 @@ import ast
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/getAllMetrics")
-def test():
+def get_all_metrics():
     with grpc.insecure_channel('microservice_2:50051') as channel:
         stub = echo_pb2_grpc.EchoServiceStub(channel)
         query_result = stub.getAllMetrics(echo_pb2.emptyParam())
@@ -23,7 +19,7 @@ def test():
         else :
             return "<p>Nessuna Metrica al momento è disponibile</p>"
 
-@app.route("/<id_metric>/metadata/")
+@app.route("/<id_metric>/metadata")
 def get_metadata_for_metrics(id_metric):
     with grpc.insecure_channel('microservice_2:50051') as channel:
         stub = echo_pb2_grpc.EchoServiceStub(channel)
@@ -34,7 +30,7 @@ def get_metadata_for_metrics(id_metric):
         else :
             return "<p>Nessuna Metrica al momento è disponibile</p>"
 
-@app.route("/<id_metric>/history/")
+@app.route("/<id_metric>/history")
 def get_history_for_metrics(id_metric):
     with grpc.insecure_channel('microservice_2:50051') as channel:
         stub = echo_pb2_grpc.EchoServiceStub(channel)
@@ -42,5 +38,16 @@ def get_history_for_metrics(id_metric):
         if len(query_result.result) != 0 :
             query_result_list = list(ast.literal_eval(query_result.result))
             return render_template('metrics_history.html', results=query_result_list)
+        else :
+            return "<p>Nessuna Metrica al momento è disponibile</p>"
+
+@app.route("/<id_metric>/prediction")
+def get_prediction_for_metrics(id_metric):
+    with grpc.insecure_channel('microservice_2:50051') as channel:
+        stub = echo_pb2_grpc.EchoServiceStub(channel)
+        query_result = stub.getPredictionForMetrics(echo_pb2.idMetricParam(idMetric=id_metric))
+        if len(query_result.result) != 0 :
+            query_result_list = list(ast.literal_eval(query_result.result))
+            return render_template('metrics_prediction.html', results=query_result_list)
         else :
             return "<p>Nessuna Metrica al momento è disponibile</p>"
